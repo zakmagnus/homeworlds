85e82c5eef52d3e80d4a69cdcb778813092fd627
ZAK Magnus <zakmagnus@gmail.com>
2021-01-19 23:38:58 -0800

    Setup method

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,11 @@ impl fmt::Display for Size {
 
 const ALL_SIZES: [Size; 3] = [Size::SMALL, Size::MEDIUM, Size::LARGE];
 
+type Piece = (Color, Size);
+
 #[derive(Debug)]
 struct Bank {
-    available_amounts: HashMap<(Color, Size), u8>,
+    available_amounts: HashMap<Piece, u8>,
 }
 
 impl Bank {
@@ -59,8 +61,16 @@ impl Bank {
         Bank { available_amounts, }
     }
 
-    pub fn num_available(&self, color: Color, size: Size) -> u8 {
-        *self.available_amounts.get(&(color, size)).unwrap()
+    pub fn num_available(&self, piece: Piece) -> u8 {
+        *self.available_amounts.get(&piece).unwrap()
+    }
+
+    pub fn remove(&mut self, piece: Piece) -> Result<(), InputError> {
+        if self.num_available(piece) < 1 {
+            return Err(InputError::PieceUnavailable);
+        }
+        *self.available_amounts.get_mut(&piece).unwrap() -= 1;
+        Ok(())
     }
 }
 
@@ -70,7 +80,7 @@ impl fmt::Display for Bank {
         for color in ALL_COLORS.iter() {
             write!(f, "{}: ", color)?;
             for (index, size) in ALL_SIZES.iter().enumerate() {
-                write!(f, "{} {}", self.num_available(*color, *size), size)?;
+                write!(f, "{} {}", self.num_available((*color, *size)), size)?;
                 if index < ALL_SIZES.len() - 1 {
                     write!(f, ", ")?;
                 }
@@ -90,6 +100,21 @@ enum State {
     Finished(PlayerIndex), // The winner's index
 }
 
+struct SetupMove {
+    player: PlayerIndex,
+    stars: [Piece; 2],
+    ship: Piece,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum InputError {
+    WrongState,
+    WrongPlayer,
+    PieceUnavailable,
+}
+
+const NUM_PLAYERS: u8 = 2;
+
 #[derive(Debug)]
 struct Game {
     state: State,
@@ -98,18 +123,41 @@ struct Game {
 }
 
 impl Game {
-    fn new() -> Game {
+    pub fn new() -> Game {
         Game {
             bank: Bank::full(),
             state: State::Setup(0),
         }
     }
+
+    pub fn setup(&mut self, setup_move: &SetupMove) -> Result<(), InputError> {
+        let player = setup_move.player;
+        match self.state {
+            State::Setup(player) => self.setup_unchecked(setup_move),
+            State::Setup(_) => Err(InputError::WrongPlayer),
+            _ => Err(InputError::WrongState),
+        }
+    }
+
+    fn setup_unchecked(&mut self, setup_move: &SetupMove) -> Result<(), InputError> {
+        self.bank.remove(setup_move.stars[0])?;
+        self.bank.remove(setup_move.stars[1])?;
+        self.bank.remove(setup_move.ship)?;
+        // TODO update systems
+        let next_player = setup_move.player + 1;
+        if next_player < NUM_PLAYERS {
+            self.state = State::Setup(next_player);
+        } else {
+            self.state = State::Turn(0);
+        }
+        Ok(())
+    }
 }
 
 fn main() {
-    let new_bank = Bank::full();
-    println!("full bank? {}", new_bank);
+    let mut game = Game::new();
+    println!("new game? {:?}\n", game);
 
-    let new_game = Game::new();
-    println!("new game? {:?}", new_game);
+    game.setup(&SetupMove{ player: 0, stars: [(Color::RED, Size::SMALL), (Color::YELLOW, Size::MEDIUM)], ship: (Color::GREEN, Size::LARGE) });
+    println!("advanced game? {:?}", game);
 }
