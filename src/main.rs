331426adbf92666e31d84f2c42848e09d1431e7b
ZAK Magnus <zakmagnus@gmail.com>
2021-01-20 19:40:05 -0800

    Adjacency

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,11 @@ impl fmt::Display for Size {
 
 const ALL_SIZES: [Size; 3] = [Size::SMALL, Size::MEDIUM, Size::LARGE];
 
-type Piece = (Color, Size);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Piece {
+    color: Color,
+    size: Size,
+}
 
 #[derive(Debug)]
 struct Bank {
@@ -55,7 +59,7 @@ impl Bank {
         let mut available_amounts = HashMap::new();
         for color in ALL_COLORS.iter() {
             for size in ALL_SIZES.iter() {
-                available_amounts.insert((*color, *size), 3);
+                available_amounts.insert(Piece {color: *color, size: *size}, 3);
             }
         }
         Bank { available_amounts, }
@@ -80,7 +84,7 @@ impl fmt::Display for Bank {
         for color in ALL_COLORS.iter() {
             write!(f, "{}: ", color)?;
             for (index, size) in ALL_SIZES.iter().enumerate() {
-                write!(f, "{} {}", self.num_available((*color, *size)), size)?;
+                write!(f, "{} {}", self.num_available(Piece {color: *color, size: *size}), size)?;
                 if index < ALL_SIZES.len() - 1 {
                     write!(f, ", ")?;
                 }
@@ -116,7 +120,7 @@ enum InputError {
 #[derive(Debug)]
 struct System {
     star: Piece,
-    other_star: Option<Piece>,
+    second_star: Option<Piece>,
     home_player: Option<PlayerIndex>,
     ships: HashMap<PlayerIndex, Vec<Piece>>,
 }
@@ -125,7 +129,7 @@ impl System {
     pub fn new_homeworld(stars: [Piece; 2], player: PlayerIndex) -> System {
         System {
             star: stars[0],
-            other_star: Some(stars[1]),
+            second_star: Some(stars[1]),
             home_player: Some(player),
             ships: System::no_ships(),
         }
@@ -134,7 +138,7 @@ impl System {
     pub fn new(star: Piece) -> System {
         System {
             star,
-            other_star: None,
+            second_star: None,
             home_player: None,
             ships: System::no_ships(),
         }
@@ -142,6 +146,28 @@ impl System {
 
     pub fn add_ship(&mut self, player: PlayerIndex, ship: Piece) {
         self.ships.get_mut(&player).unwrap().push(ship);
+    }
+
+    pub fn is_adjacent(&self, other_system: &System) -> bool {
+        if self.star.size == other_system.star.size {
+            return false;
+        }
+        if let Some(second_star) = self.second_star {
+            if second_star.size == other_system.star.size {
+                return false;
+            }
+        }
+        if let Some(other_second_star) = other_system.second_star {
+            if self.star.size == other_second_star.size {
+                return false;
+            }
+            if let Some(second_star) = self.second_star {
+                if second_star.size == other_second_star.size {
+                    return false;
+                }
+            }
+        }
+        true
     }
 
     fn no_ships() -> HashMap<PlayerIndex, Vec<Piece>> {
@@ -203,6 +229,34 @@ fn main() {
     let mut game = Game::new();
     println!("new game? {:?}\n", game);
 
-    game.setup(&SetupMove{ player: 0, stars: [(Color::RED, Size::SMALL), (Color::YELLOW, Size::MEDIUM)], ship: (Color::GREEN, Size::LARGE) });
+    game.setup(&SetupMove{ player: 0, stars: [Piece {color: Color::RED, size: Size::SMALL}, Piece{color: Color::YELLOW, size: Size::MEDIUM}], ship: Piece{color: Color::GREEN, size: Size::LARGE} });
     println!("advanced game? {:?}", game);
+
+    let big = System::new(Piece {color: Color::RED, size: Size::LARGE});
+    let medium = System::new(Piece {color: Color::RED, size: Size::MEDIUM});
+    let small = System::new(Piece {color: Color::RED, size: Size::SMALL});
+    let homeworld = game.systems.get(0).unwrap();
+    println!("b b {}", big.is_adjacent(&big));
+    println!("b m {}", big.is_adjacent(&medium));
+    println!("b s {}", big.is_adjacent(&small));
+    println!("b h {}", big.is_adjacent(homeworld));
+
+    println!("m b {}", medium.is_adjacent(&big));
+    println!("m m {}", medium.is_adjacent(&medium));
+    println!("m s {}", medium.is_adjacent(&small));
+    println!("m h {}", medium.is_adjacent(homeworld));
+
+    println!("s b {}", small.is_adjacent(&big));
+    println!("s m {}", small.is_adjacent(&medium));
+    println!("s s {}", small.is_adjacent(&small));
+    println!("s h {}", small.is_adjacent(homeworld));
+
+    println!("h b {}", homeworld.is_adjacent(&big));
+    println!("h m {}", homeworld.is_adjacent(&medium));
+    println!("h s {}", homeworld.is_adjacent(&small));
+    println!("h h {}", homeworld.is_adjacent(homeworld));
+
+    let hw2 = System::new_homeworld([Piece {color: Color::RED, size: Size::MEDIUM}, Piece {color: Color::RED, size: Size::LARGE}], 0);
+    println!("h1 h2 {}", homeworld.is_adjacent(&hw2));
+    println!("h2 h1 {}", hw2.is_adjacent(homeworld));
 }
